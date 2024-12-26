0bddb3ad8c19aed7866497ae8fc84eacaf88ac3a
Romzi Farhan Ghozi <romzifg@gmail.com>
2024-12-26 07:08:52 +0700

    update: create module city & car service

---
+++ app/Filament/Resources/CarServiceResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CarServiceResource\Pages;
+use App\Filament\Resources\CarServiceResource\RelationManagers;
+use App\Models\CarService;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CarServiceResource extends Resource
+{
+    protected static ?string $model = CarService::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR'),
+
+                Forms\Components\TextInput::make('duration_in_hour')
+                    ->required()
+                    ->numeric()
+                    ->maxLength(255),
+
+                Forms\Components\FileUpload::make('photo')
+                    ->image()
+                    ->required(),
+
+                Forms\Components\Textarea::make('about')
+                    ->required()
+                    ->rows(10)
+                    ->cols(20)
+                    ->maxLength(255),
+
+                Forms\Components\FileUpload::make('icon')
+                    ->image()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+            Tables\Columns\TextColumn::make('price'),
+            Tables\Columns\ImageColumn::make('icon'),
+            Tables\Columns\ImageColumn::make('photo'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCarServices::route('/'),
+            'create' => Pages\CreateCarService::route('/create'),
+            'edit' => Pages\EditCarService::route('/{record}/edit'),
+        ];
+    }
+}
